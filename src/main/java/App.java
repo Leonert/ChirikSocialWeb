b199ef3d6b01b14d4520a0a2b99ce35db0f42717
Leonert <leonid.knyshev@gmail.com>
2023-04-18 01:14:46 +0300

    init

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world!");
     }
 
     public static boolean dummy() {
