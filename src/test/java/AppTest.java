f61363738b1cce8c5bf713f09b4a2f95d733ef9a
Leonert <leonid.knyshev@gmail.com>
2023-04-17 23:59:49 +0300

    init

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    void basicTest() {
+        Assertions.assertTrue(App.dummy());
+    }
+}
