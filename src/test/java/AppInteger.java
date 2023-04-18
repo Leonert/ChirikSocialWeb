0b552573ff54b269a049a697c9776244b2450ce8
Leonert <121697087+Leonert@users.noreply.github.com>
2023-04-18 05:37:08 +0300

    Us6 (#2)
    init

---
+++ src/test/java/AppInteger.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppInteger {
+    @Test
+    void basicTest() {
+        Assertions.assertTrue(App.dummy());
+    }
+}
