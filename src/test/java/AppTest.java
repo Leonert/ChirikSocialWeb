a7ce4fe128fab2c0dd3842dad4e2eba30620a402
Leonert <leonid.knyshev@gmail.com>
2023-04-18 15:54:42 +0300

    add coveralls support

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class AppInteger {
+public class AppTest {
     @Test
     void basicTest() {
         Assertions.assertTrue(App.dummy());
