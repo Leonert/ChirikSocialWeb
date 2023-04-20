468028b3451ae79361b477e58913ae4fa0b55b43
mt_tarik <skorohodit12@gmail.com>
2023-04-20 12:29:51 +0300

    [US2] - delete file src

--- src/test/java/AppInteger.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-public class AppInteger {
-    @Test
-    void basicTest() {
-        Assertions.assertTrue(App.dummy());
-    }
-}
