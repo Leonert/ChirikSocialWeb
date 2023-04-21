f45244688e33d111e97b7fcb397d8b2d39307a49
Anton Bakhtin <96262769+bymooon@users.noreply.github.com>
2023-04-21 20:58:41 +0300

    [US18] Create universal components Button and Modal. (#18)
    * Create universal component Button
    
    * Create universal component Modal
    
    * Install MUI icons package

---
+++ social-network-ui/src/components/UI/Modal.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import MuiDialog from '@mui/material/Dialog';
+import { DialogContent, DialogTitle, IconButton, Typography, styled } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+const CustomModal = styled(MuiDialog)({
+  '.MuiDialog-paper': {
+    backgroundColor: 'black',
+    maxWidth: '600px',
+    width: '100%',
+    borderRadius: '8px',
+    color: 'white',
+  },
+});
+
+const Modal = ({ children, open, onClose, headerText, hasLogoIcon = true, sx }) => {
+  return (
+    <CustomModal
+      PaperProps={{ sx }}
+      open={open}
+      onClose={onClose}
+    >
+      <>
+        <DialogTitle sx={{ display: 'grid', justifyItems: 'start', gridTemplateColumns: 'repeat(3, 1fr)' }}>
+          <IconButton
+            sx={{ p: 0 }}
+            onClick={onClose}
+          >
+            <CloseIcon sx={{ color: 'white' }} />
+          </IconButton>
+          {headerText ? (
+            <Typography sx={{ justifySelf: 'center' }}> {headerText}</Typography>
+          ) : (
+            hasLogoIcon && (
+              <TwitterIcon
+                sx={{ display: 'block', color: 'white', width: '32px', height: '32px', justifySelf: 'center' }}
+              />
+            )
+          )}
+        </DialogTitle>
+        <DialogContent> {children}</DialogContent>
+      </>
+    </CustomModal>
+  );
+};
+
+export default Modal;
