e69b6777d7b2b6b8ea2f8eb86d6f53e74476221c
mt_tarik <skorohodit12@gmail.com>
2023-04-22 10:49:20 +0300

    [US8] - Downloaded Created and configured the ESLint file

--- social-network-ui/src/App.js
+++ social-network-ui/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
+import Button from "./components/UI/Button";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+
+
+  </div>;
 }
 
 export default App;
