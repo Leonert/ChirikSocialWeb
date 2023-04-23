f9c59d85597b73539d1ef0a89825359de0610566
Izmailov Mykyta <lampysk8@gmail.com>
2023-04-23 18:39:07 +0300

    [US8] Configure ESLint and Stylelint for FE.

--- social-network-ui/src/index.js
+++ social-network-ui/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/router';
+
+import { store } from './app/store';
 import './index.css';
+import { router } from './routes/router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
