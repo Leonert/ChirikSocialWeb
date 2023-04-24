6212c1b7cb0409f3d57e19648bdd0e2c7d13ea6a
Leonert <leonid.knyshev@gmail.com>
2023-04-24 13:31:59 +0300

    add cors mapping for web configuration and optional cors request allowing due to possible spring secure implementation

---
+++ social-network-api/src/main/java/com/socialnetwork/api/configs/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+// To add in case of Spring Security realization
+//
+// package com.socialnetwork.api.configs;
+//
+//import org.springframework.context.annotation.Bean;
+//
+//@EnableWebSecurity
+//public class WebSecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.cors();
+//        return http.build();
+//    }
+//}
