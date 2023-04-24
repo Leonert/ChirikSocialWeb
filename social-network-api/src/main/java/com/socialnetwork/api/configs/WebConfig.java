bdb31fd68267a335d2e6f54630417f967e1e804a
Leonert <leonid.knyshev@gmail.com>
2023-04-24 14:56:24 +0300

    change indents

--- social-network-api/src/main/java/com/socialnetwork/api/configs/WebConfig.java
+++ social-network-api/src/main/java/com/socialnetwork/api/configs/WebConfig.java
@@ -8,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**");
     }
 }
