d2e5457bab9ebee1cfde3d2406b62f4e821ec9f5
Anton <bahtin.antonik@gmail.com>
2023-04-27 21:04:34 +0300

    Create AccountProfileFields  component in RegistrationModal.

---
+++ social-network-ui/src/components/Registration/AccountProfileFields.jsx
@@ -0,0 +1,87 @@
+import { Box, TextField, Typography } from '@mui/material';
+import React from 'react';
+
+import Button from '../UI/Button';
+
+const AccountProfileFields = ({ formik, onSubmit }) => {
+  const handleEmailBlur = (e) => {
+    console.log('emailLostFocus');
+    formik.handleBlur(e);
+  };
+
+  const handleUsernameBlur = (e) => {
+    console.log('NicknameLostFocus');
+    formik.handleBlur(e);
+  };
+
+  const handleSubmit = () => {
+    onSubmit();
+  };
+
+  return (
+    <Box
+      sx={{
+        maxWidth: '438px',
+        margin: 'auto',
+        width: '100%',
+      }}
+    >
+      <Typography sx={{ fontWeight: 700, fontSize: 32 }}>Create your profile</Typography>
+      <TextField
+        sx={{ mt: '20px', mb: '20px' }}
+        fullWidth
+        id="email"
+        name="email"
+        label="Email"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        error={formik.touched.email && Boolean(formik.errors.email)}
+        helperText={formik.touched.email && formik.errors.email}
+        onBlur={handleEmailBlur}
+      />
+      <TextField
+        sx={{ mb: '20px' }}
+        fullWidth
+        id="username"
+        name="username"
+        label="Username"
+        value={formik.values.username}
+        onChange={formik.handleChange}
+        error={formik.touched.username && Boolean(formik.errors.username)}
+        helperText={formik.touched.username && formik.errors.username}
+        onBlur={handleUsernameBlur}
+      />
+      <TextField
+        sx={{ mb: '20px' }}
+        fullWidth
+        id="password"
+        name="password"
+        label="Password"
+        type="password"
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        error={formik.touched.password && Boolean(formik.errors.password)}
+        helperText={formik.touched.password && formik.errors.password}
+        onBlur={formik.handleBlur}
+      />
+      <TextField
+        sx={{ mb: '20px' }}
+        fullWidth
+        id="repeatPassword"
+        name="repeatPassword"
+        label="Repeat password"
+        type="password"
+        value={formik.values.repeatPassword}
+        onChange={formik.handleChange}
+        error={formik.touched.repeatPassword && Boolean(formik.errors.repeatPassword)}
+        helperText={formik.touched.repeatPassword && formik.errors.repeatPassword}
+        onBlur={formik.handleBlur}
+      />
+      <Button color="primary" variant="contained" fullWidth onClick={onSubmit}>
+        Next
+      </Button>
+    </Box>
+  );
+};
+
+export default AccountProfileFields;
