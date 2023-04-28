1e14040a9e8951068f8fbd0fde24994c735826e9
^Taras <82092541+TarasSkorohod@users.noreply.github.com>
2023-04-28 16:44:27 +0300

    [US17] Create header menu (#26)
    * [US17] - create SideMenu - create theme.js create text icon Twit
    
    * [US17] - create Modal Twit button style globalClasses.jsx side menu final
    
    * [US17] - revision of the code to clean up the excess
    
    * [US17] - App app conflict
    
    * [US17] - EsList fix Bugs
    
    * [US17] - fix Bugs
    
    * [US17] - fix Bugs
    
    * [US17] - fix Bugs
    
    * [US17] - fix Bugs
    
    * [US17] - fix Bugs
    
    * [US17] - fix
    
    * [US17] - fix_ ci.yml
    
    * [US17] - fix_ ci.yml
    
    * [US17] - fix_ ci.yml
    
    * [US17] - fix_ ci.yml
    
    * [US17] - fix_eslint
    
    * [US17] - fix_eslint
    
    * [US17] - fix ci.yml
    
    * [US17] - fix ci.yml
    
    * [US17] - fix
    
    * [US17] - fix
    
    * [US17] - fix package.json
    
    * [US17] - fix
    
    * [US17] - fix
    
    * [US17] - fix
    
    * [US17] - fix_goo
    
    * [US17] - fix_goo
    
    * [US17] - test_test)

---
+++ social-network-ui/src/components/SideMenu/AddTweetModal/AddTweetModalStyles.jsx
@@ -0,0 +1,24 @@
+import { makeStyles, Theme } from "@material-ui/core";
+
+export const useAddTweetModalStyles = makeStyles((theme: Theme) => ({
+    content: {
+        top: "-20%",
+        "& .MuiDialogTitle-root": {
+            padding: "5px 15px",
+            marginBottom: 0,
+            borderBottom: `1px solid ${theme.palette.divider}`
+        }
+    },
+    header: {
+        padding: "5px 15px",
+        margin: 0,
+        "& svg": {
+            fontSize: 26
+        }
+    },
+    dialogContent: {
+        width: 598,
+        minHeight: 288,
+        padding: "10px 20px 10px 20px"
+    }
+}));
