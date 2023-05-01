206125ed4a6d23c8d7a9e8d4e76da9486a8608cc
Leonert <leonid.knyshev@gmail.com>
2023-05-01 14:12:55 +0300

    message

--- social-network-api/src/main/java/com/socialnetwork/api/controller/PostController.java
+++ social-network-api/src/main/java/com/socialnetwork/api/controller/PostController.java
@@ -23,6 +23,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
 
