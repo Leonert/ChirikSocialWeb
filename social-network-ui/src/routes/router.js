8bb35f94b10768a3f7cd09634bd41c3a06fea977
mt_tarik <skorohodit12@gmail.com>
2023-05-03 14:23:19 +0300

    [US26]- Fix go to main

--- social-network-ui/src/routes/router.js
+++ social-network-ui/src/routes/router.js
@@ -5,9 +5,6 @@ import { Layout } from '../layout/Layout';
 import {MuiThemeProvider} from "@material-ui/core";
 import { defaultTheme} from "../theme";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Messages from "../pages/Messages/Messages";
-import Home from "../pages/Home/Home";
-import {HOME, SEARCH} from "../util/path-constants";
 
 export const theme= (defaultTheme);
 export const router = createBrowserRouter([
@@ -19,42 +16,5 @@ export const router = createBrowserRouter([
             <Layout />
         </MuiThemeProvider>,
     errorElement: <div>Error page</div>,
-    children: [
-      {
-        path: HOME,
-        element:
-          <Home />
-      },
-      {
-          path: SEARCH,
-          element:
-           <Home />
-      },
-      {
-          path: '/notifications',
-          element:
-              <Home />
-      },
-      {
-          path: '/messages',
-          element:
-             <Messages />
-      },
-      {
-          path: '/bookmarks',
-          element:
-              <Home />
-      },
-      {
-          path: '/lists',
-          element:
-              <Home />
-      },
-      {
-          path: '/profile',
-          element:
-              <Home />
-      },
-    ],
   },
 ]);
