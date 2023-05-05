16f4da52f858731346c1fee91a2d71641e1edf41
Anton <bahtin.antonik@gmail.com>
2023-05-05 17:13:01 +0300

    Create BottomLine component for user auth.

---
+++ social-network-ui/src/components/BottomLine/BottomLine.jsx
@@ -0,0 +1,28 @@
+import { Box } from '@mui/material';
+
+import RegistrationModal from '../Registration/RegistrationModal';
+
+export const BottomLine = () => {
+  return (
+    <Box
+      sx={{
+        width: '100%',
+      }}
+    >
+      <Box
+        sx={{
+          width: '100%',
+          padding: '20px',
+          position: 'fixed',
+          bottom: 0,
+          backgroundColor: 'blue',
+          display: 'flex',
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+        }}
+      >
+        <RegistrationModal />
+      </Box>
+    </Box>
+  );
+};
