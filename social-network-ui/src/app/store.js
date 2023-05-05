3d2afa748b930b30a1a449c025899ac9f3c11adb
Anton <bahtin.antonik@gmail.com>
2023-05-05 17:22:48 +0300

    Add testReducer to store.

--- social-network-ui/src/app/store.js
+++ social-network-ui/src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { testReducer } from '../features/slices/testSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    test: testReducer,
+  },
 });
