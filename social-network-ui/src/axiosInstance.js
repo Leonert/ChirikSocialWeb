468fba2a6f0c39e4834649560e9d40e64ef5a953
Anton <bahtin.antonik@gmail.com>
2023-05-05 17:07:13 +0300

    Create axios config

---
+++ social-network-ui/src/axiosInstance.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const axiosIns = axios.create({
+  baseURL: 'http://localhost:8080',
+});
+
+axiosIns.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      config.headers.Authorization = token;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosIns.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401) {
+      localStorage.removeItem('token');
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default axiosIns;
