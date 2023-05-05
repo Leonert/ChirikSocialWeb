56916397b2d78985eed75417b44b542caf4a53a6
Anton <bahtin.antonik@gmail.com>
2023-05-05 21:26:49 +0300

    Fix MUI default theme.

--- social-network-ui/src/index.js
+++ social-network-ui/src/index.js
@@ -1,3 +1,4 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -6,10 +7,14 @@ import { RouterProvider } from 'react-router-dom';
 import { store } from './app/store';
 import './index.css';
 import { router } from './routes/router';
+import { defaultTheme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <RouterProvider router={router} />
-  </Provider>
+  <ThemeProvider theme={defaultTheme}>
+    <CssBaseline />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </ThemeProvider>
 );
