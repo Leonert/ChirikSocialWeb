775fcee5b62612061e15fe296acdb198fb4ad5fd
Oleksandr Klimenchenko <himok.com@gmail.com>
2023-05-08 18:50:28 +0300

    fix problem

--- social-network-ui/src/components/ActionIconButton/ActionIconButton.jsx
+++ social-network-ui/src/components/ActionIconButton/ActionIconButton.jsx
@@ -8,22 +8,22 @@ const HOVER_DELAY = 500;
 const ActionIconButton = ({ id, onClick, icon, size = 'small', disabled }) => {
   const classes = useActionIconButtonStyles();
   const [delayHandler, setDelayHandler] = useState(null);
-  const [visibleHoverAction, setVisibleHoverAction] = useState(false);
+  const setVisibleHoverAction = false;
 
-  const handleHoverAction = () => {
-    setDelayHandler(setTimeout(() => setVisibleHoverAction(true), HOVER_DELAY));
-  };
+  // const handleHoverAction = () => {
+  //   setDelayHandler(setTimeout(() => setVisibleHoverAction(true), HOVER_DELAY));
+  // };
 
   const handleLeaveAction = () => {
     clearTimeout(delayHandler);
-    setVisibleHoverAction(false);
+    setVisibleHoverAction = false;
   };
 
   return (
     <div id={id} className={classes.icon}>
       <IconButton
         onClick={onClick}
-        onMouseEnter={handleHoverAction}
+        // onMouseEnter={handleHoverAction}
         onMouseLeave={handleLeaveAction}
         disabled={disabled}
         color="primary"
