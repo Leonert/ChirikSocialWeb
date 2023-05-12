313b790a5f83bc63a175dd3000cf057a286c906c
mt_tarik <skorohodit12@gmail.com>
2023-05-12 21:49:58 +0300

    [B7.1] - commit fix bug pull

--- social-network-api/src/main/java/com/socialnetwork/api/controller/MessagesController.java
+++ social-network-api/src/main/java/com/socialnetwork/api/controller/MessagesController.java
@@ -7,9 +7,15 @@ import com.socialnetwork.api.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
--- social-network-api/src/main/java/com/socialnetwork/api/models/base/Message.java
+++ social-network-api/src/main/java/com/socialnetwork/api/models/base/Message.java
@@ -1,9 +1,17 @@
 package com.socialnetwork.api.models.base;
 
+
 import lombok.Data;
 
-
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.SequenceGenerator;
 import java.time.LocalDateTime;
 
 @Data
