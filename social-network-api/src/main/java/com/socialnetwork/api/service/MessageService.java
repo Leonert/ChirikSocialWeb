3aaeb48c3f0db77c896937959fdd1ef483e88509
mt_tarik <skorohodit12@gmail.com>
2023-05-13 12:52:11 +0300

    [B7.1] - update

--- social-network-api/src/main/java/com/socialnetwork/api/service/MessageService.java
+++ social-network-api/src/main/java/com/socialnetwork/api/service/MessageService.java
@@ -5,12 +5,19 @@ import com.socialnetwork.api.dto.MessageDto;
 import java.util.List;
 
 public interface MessageService {
+
   MessageDto getMessageById(int id);
+
   List<MessageDto> getAllMessages();
+
   MessageDto createMessage(MessageDto messageDto);
+
   MessageDto updateMessage(MessageDto messageDto);
+
   void deleteMessage(int id);
+
   List<MessageDto> searchMessages(String keyword);
+
   void markAsRead(int id);
 
 }
