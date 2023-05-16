0c6c52f43686f16e7a378b4593ac37199ddef6a3
Oleksandr Skyrda <alex.skirda@gmail.com>
2023-05-16 16:49:58 +0300

    added oauth2 classes

--- social-network-api/src/main/java/com/socialnetwork/api/Application.java
+++ social-network-api/src/main/java/com/socialnetwork/api/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-//@EnableConfigurationProperties(AppProperties.class)
+@EnableConfigurationProperties(AppProperties.class)
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class);
