2c1109d8170f7e92b77dedc2defef5e569e8bc36
Anton <bahtin.antonik@gmail.com>
2023-05-17 18:33:28 +0300

    Added validation for an existing email and username.

--- social-network-ui/src/components/Registration/RegistrationModal.jsx
+++ social-network-ui/src/components/Registration/RegistrationModal.jsx
@@ -2,7 +2,7 @@ import { useFormik } from 'formik';
 import React, { useState } from 'react';
 import * as yup from 'yup';
 
-// import axiosIns from '../../axiosInstance';
+import axiosIns from '../../axiosInstance';
 import Button from '../UI/Button';
 import Modal from '../UI/Modal';
 import AccountProfileFields from './AccountProfileFields';
@@ -10,8 +10,53 @@ import BIOFields from './BIOFields';
 import ConfirmMail from './ConfirmMail';
 import InitialStep from './InitialStep';
 
+async function emailAsyncValidation(email) {
+  const response = await axiosIns.get('/api/registration/email', {
+    params: { e: email },
+  });
+
+  return response;
+}
+
+async function usernameAsyncValidation(username) {
+  const response = await axiosIns.get('/api/registration/username', {
+    params: { u: username },
+  });
+
+  return response;
+}
+
 const accountProfileValidationSchema = yup.object({
-  email: yup.string('Enter your email').email('Enter a valid email').required('Email is required'),
+  email: yup
+    .string('Enter your email')
+    .email('Enter a valid email')
+    .required('Email is required')
+    .test('Email already exists', 'Email already exists', async (email) => {
+      let isUnique = false;
+      try {
+        const response = await emailAsyncValidation(email);
+        isUnique = response;
+      } catch (e) {
+        // console.error('Error: ', e.response.status);
+      }
+
+      return isUnique;
+    }),
+  username: yup
+    .string('Enter your username')
+    .min(4, 'Username should be minimum 4 characters length')
+    .required('Username is required')
+    .test('Username already exists', 'Username already exists', async (username) => {
+      let isUnique = false;
+      try {
+        const response = await usernameAsyncValidation(username);
+        isUnique = response;
+      } catch (e) {
+        // console.error('Error: ', e.response.status);
+      }
+
+      return isUnique;
+    }),
   password: yup
     .string('Enter your password')
     .min(8, 'Password should be minimum 8 characters length')
@@ -21,10 +66,6 @@ const accountProfileValidationSchema = yup.object({
     .min(8, 'Password should be minimum 8 characters length')
     .required('Password is required')
     .oneOf([yup.ref('password'), null], 'Passwords must match'),
-  username: yup
-    .string('Enter your username')
-    .min(4, 'Username should be minimum 4 characters length')
-    .required('Username is required'),
 });
 
 const maxValidDate = new Date(new Date().getTime() + 1 * 60000);
@@ -68,17 +109,30 @@ const RegistrationModal = () => {
       }
     } else {
       handleNextStep();
+      const date = formik.values.birthDate;
+
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const day = String(date.getDate()).padStart(2, '0');
+      const hours = String(date.getHours()).padStart(2, '0');
+      const minutes = String(date.getMinutes()).padStart(2, '0');
+      const seconds = String(date.getSeconds()).padStart(2, '0');
+
+      const formattedBirthDate = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
       try {
-        // const data = {
-        //   emailAddress: values.email,
-        //   username: values.username,
-        //   password: values.password,
-        //   firstName: values.name,
-        //   lastName: values.surname,
-        //   birthDate: values.birthDate,
-        // };
-        // const response = await axiosIns.post('/api/registration/save-user', data);
-      } catch (e) {}
+        const data = {
+          emailAddress: formik.values.email,
+          username: formik.values.username,
+          password: formik.values.password,
+          name: formik.values.name + ' ' + formik.values.surname,
+          birthDate: formattedBirthDate,
+        };
+        await axiosIns.post('/api/registration', {
+          body: data,
+        });
+      } catch (e) {
+        // console.error('Error: ', e.response.status);
+      }
     }
   };
 
@@ -107,10 +161,6 @@ const RegistrationModal = () => {
   const handleClose = () => {
     setOpen(false);
   };
-
-  // const handlePrevStep = () => {
-  //   setActiveStep((prevStep) => prevStep - 1);
-  // };
 
   const steps = [
     <InitialStep key={1} onCreateAccount={handleNextStep} />,
