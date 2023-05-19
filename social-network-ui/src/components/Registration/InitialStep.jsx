b7c4f8c786215b92f4457f74424474ec4a77970a
Anton <bahtin.antonik@gmail.com>
2023-05-19 16:03:59 +0300

    Add logic to init step.

--- social-network-ui/src/components/Registration/InitialStep.jsx
+++ social-network-ui/src/components/Registration/InitialStep.jsx
@@ -1,7 +1,7 @@
-import { Box, Divider, Link, Typography } from '@mui/material';
 import GoogleIcon from '@mui/icons-material/Google';
-
+import { Box, Divider, Link, Typography } from '@mui/material';
 import React from 'react';
+
 import Button from '../UI/Button';
 
 const InitialStep = (props) => {
@@ -15,22 +15,16 @@ const InitialStep = (props) => {
       }}
     >
       <Typography sx={{ fontSize: '36px', marginTop: '10px', marginBottom: '20px' }}>Join Twitter now! </Typography>
-      <Button
-        startIcon={<GoogleIcon />}
-        sx={{ maxWidth: '300px', width: '100%', marginBottom: '12px' }}
-      >
+      <Button startIcon={<GoogleIcon />} sx={{ maxWidth: '300px', width: '100%', marginBottom: '12px' }}>
         <Typography sx={{ fontSize: '14px' }}> Register with Google </Typography>
       </Button>
       <Divider sx={{ '&::after, &::before': { borderColor: 'rgba(255, 255, 255, 0.4)' }, marginBottom: '12px' }}>
         or
       </Divider>
-      <Button
-        onClick={props.onCreateAccount}
-        sx={{ maxWidth: '300px', width: '100%', marginBottom: '30px' }}
-      >
+      <Button onClick={props.onCreateAccount} sx={{ maxWidth: '300px', width: '100%', marginBottom: '30px' }}>
         <Typography sx={{ fontSize: '14px' }}> Registration </Typography>
       </Button>
-      <Typography sx={{ fontSize: '14px' }}>
+      <Typography onClick={props.handleClose} sx={{ fontSize: '14px' }}>
         Already have an account? <Link sx={{ cursor: 'pointer' }}>Sign in</Link>
       </Typography>
     </Box>
