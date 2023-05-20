f41ede7d7db44e5c1f91bcb0ea6d74cd839ac416
Anton <bahtin.antonik@gmail.com>
2023-05-20 22:51:19 +0300

    Add request to user profile.

--- social-network-ui/src/pages/Profile/profileLoader.js
+++ social-network-ui/src/pages/Profile/profileLoader.js
@@ -1,24 +1,17 @@
-const data = {
-  firstName: 'Misha',
-  lastName: 'Romanenko',
-  birthDate: new Date(2020, 10, 10).toISOString().split('T')[0],
-  accCreateDate: '20/10/2020',
-  username: 'username',
-  subscriberNumber: 20,
-  followNumber: 50,
-  userTweets: {
-    username: 'tweeUsername',
-    postID: 1,
-  },
-  BIO: 'BIO',
-  location: 'Kyiv',
-  website: 'instagram.com',
-  avatar: 'https://nftnow.com/wp-content/uploads/2022/08/pudgy-penguin-6873.png',
-  profileBackground: '',
-};
+import { json } from 'react-router-dom';
+
+import axiosIns from '../../axiosInstance';
 
 const profileLoader = async ({ params }) => {
-  return data;
+  try {
+    const response = await axiosIns.get(`/api/users/p/${params.username}`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    });
+
+    return response;
+  } catch (e) {
+    return json({ Error: e });
+  }
 };
 
 export default profileLoader;
