f8a62182b88f686e40f4408771497d40dddb4f2e
CoffeeScriptHero <denis_kozarenko@ukr.net>
2023-05-20 19:14:20 +0300

    [F20.1] fixes

--- social-network-ui/src/pages/Home/Home.jsx
+++ social-network-ui/src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import Grid from '@mui/material/Grid';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import ButtonShowMore from '../../components/ButtonShowMore/ButtonShowMore';
