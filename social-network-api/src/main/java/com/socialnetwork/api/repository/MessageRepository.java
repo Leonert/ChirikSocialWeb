63f5bffbbe126ae68fff66fb88df7e546af5e8e9
mt_tarik <skorohodit12@gmail.com>
2023-05-20 10:04:07 +0300

    [b7.1]- fix

--- social-network-api/src/main/java/com/socialnetwork/api/repository/MessageRepository.java
+++ social-network-api/src/main/java/com/socialnetwork/api/repository/MessageRepository.java
@@ -18,7 +18,6 @@ public interface MessageRepository extends JpaRepository<Message, Integer> {
   List<Message> findBySender(User sender);
 
   List<Message> findByMessageContainingIgnoreCase(String keyword);
-
   @Transactional
   @Modifying
   @Query("DELETE FROM Message m WHERE m.id = :id")
