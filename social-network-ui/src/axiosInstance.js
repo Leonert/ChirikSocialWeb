decc41fc4c98c5996ad1364afb04ca296561ce4c
Anton <bahtin.antonik@gmail.com>
2023-05-22 15:40:02 +0300

    Add constant to token.

--- social-network-ui/src/axiosInstance.js
+++ social-network-ui/src/axiosInstance.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
+import { TOKEN } from './util/constants';
+
 const axiosIns = axios.create({
   baseURL: 'http://localhost:8080',
 });
 
 axiosIns.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem(TOKEN);
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
