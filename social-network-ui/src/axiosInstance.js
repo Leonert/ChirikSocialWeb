6d032a19973a56a332dbe02592cc735ec2358ec2
Anton Bakhtin <96262769+bymooon@users.noreply.github.com>
2023-05-22 16:28:01 +0300

    [F22.1] Hotfix "token" and "jwt" ambiguous (#61)
    * Fix, change setItem name from jwt to token.
    
    * Trim bearer token from jwt.
    
    * Fix api reference for users.
    
    * Add constant to token.
    
    * Replace token on constant.
    
    * Create constants.

--- social-network-ui/src/axiosInstance.js
+++ social-network-ui/src/axiosInstance.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
+import { TOKEN } from './util/constants';
+
 const axiosIns = axios.create({
   baseURL: 'http://localhost:8080',
 });
 
 axiosIns.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem(TOKEN);
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
@@ -21,7 +23,7 @@ axiosIns.interceptors.request.use(
 //   (response) => response,
 //   (error) => {
 //     if (error.response.status === 401) {
-//       localStorage.removeItem('token');
+//       localStorage.removeItem(TOKEN);
 //     }
 
 //     return Promise.reject(error);
