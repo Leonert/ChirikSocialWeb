0470438583d110d5a3d37e43c79617bca9a52bad
Anton <bahtin.antonik@gmail.com>
2023-05-24 11:52:00 +0300

    Delete header of request.

--- social-network-ui/src/pages/Profile/profileLoader.js
+++ social-network-ui/src/pages/Profile/profileLoader.js
@@ -4,9 +4,7 @@ import axiosIns from '../../axiosInstance';
 
 const profileLoader = async ({ params }) => {
   try {
-    const response = await axiosIns.get(`/api/users/p/${params.username}`, {
-      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-    });
+    const response = await axiosIns.get(`/api/users/p/${params.username}`);
 
     return response;
   } catch (e) {
