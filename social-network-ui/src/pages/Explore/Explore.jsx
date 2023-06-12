6702a29135e9773ae5bda8770b732e6b05b261ab
Anton <bahtin.antonik@gmail.com>
2023-06-12 17:07:31 +0300

    Add trends to explore page

--- social-network-ui/src/pages/Explore/Explore.jsx
+++ social-network-ui/src/pages/Explore/Explore.jsx
@@ -1,15 +1,16 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid } from '@mui/material';
 import React from 'react';
 
 import AsideRecommendFollows from '../../components/AsideRecommendFollows/AsideRecommendFollows';
 import SearchField from '../../components/SearchField/SearchField';
+import Trends from './Trends';
 
 const Explore = () => {
   return (
     <Grid container spacing={2}>
       <Grid item xs={7}>
         <SearchField />
-        <Typography>Trends for you</Typography>
+        <Trends/>
       </Grid>
       <Grid item xs={5}>
         <AsideRecommendFollows />
