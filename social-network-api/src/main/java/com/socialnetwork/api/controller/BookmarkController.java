82f96417d0beb7c766017c154ad194e32f7790aa
Denys Kozarenko <80705582+CoffeeScriptHero@users.noreply.github.com>
2023-06-16 19:35:45 +0300

    [B8.2] Spring Security configured (#84)

--- social-network-api/src/main/java/com/socialnetwork/api/controller/BookmarkController.java
+++ social-network-api/src/main/java/com/socialnetwork/api/controller/BookmarkController.java
@@ -6,18 +6,17 @@ import com.socialnetwork.api.exception.custom.NoUserWithSuchCredentialsException
 import com.socialnetwork.api.mapper.authorized.PostMapper;
 import com.socialnetwork.api.models.additional.Bookmark;
 import com.socialnetwork.api.models.base.Post;
+import com.socialnetwork.api.security.CurrentUser;
+import com.socialnetwork.api.security.JwtUserDetails;
 import com.socialnetwork.api.service.authorized.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.socialnetwork.api.util.Constants.Auth.USERNAME_ATTRIBUTE;
 
 @RestController
 @RequestMapping("/api/bookmarks")
@@ -27,9 +26,10 @@ public class BookmarkController {
   private final PostMapper postMapper;
 
   @GetMapping()
-  public ResponseEntity<List<PostDtoInterface>> getUserNotifications(@RequestAttribute(USERNAME_ATTRIBUTE) String username)
+  public ResponseEntity<List<PostDtoInterface>> getUserNotifications(@CurrentUser JwtUserDetails currentUser)
         throws NoUserWithSuchCredentialsException, NoPostWithSuchIdException {
     List<PostDtoInterface> outcome = new ArrayList<>();
+    String username = currentUser.getUsername();
     for (Post post : userService.findByUsername(username).getBookmarkedPosts()
           .stream().map(Bookmark::getBookmarkedPost).toList()) {
       outcome.add(postMapper.convertToPostDtoDefault(post, username));
