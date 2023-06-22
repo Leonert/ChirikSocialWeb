b4ce384f743f64b7fafdc86a2d4dbb8d8e335f1b
mt_tarik <skorohodit12@gmail.com>
2023-06-22 11:49:22 +0300

    fix_bug

--- social-network-api/src/main/java/com/socialnetwork/api/configs/WebSocketConfiguration.java
+++ social-network-api/src/main/java/com/socialnetwork/api/configs/WebSocketConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -13,9 +12,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-
         config.enableSimpleBroker("/topic");
-
         config.setApplicationDestinationPrefixes("/app");
     }
 
@@ -23,5 +20,4 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/websocket").setAllowedOriginPatterns("*").withSockJS();
     }
-
 }
