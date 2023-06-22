6db3b1428809e8c4252fd421e6a0f885e5b315c1
mt_tarik <skorohodit12@gmail.com>
2023-06-22 12:07:05 +0300

    fix_bug

--- social-network-api/src/main/java/com/socialnetwork/api/configs/WebSocketConfiguration.java
+++ social-network-api/src/main/java/com/socialnetwork/api/configs/WebSocketConfiguration.java
@@ -9,11 +9,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
     }
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/websocket")
