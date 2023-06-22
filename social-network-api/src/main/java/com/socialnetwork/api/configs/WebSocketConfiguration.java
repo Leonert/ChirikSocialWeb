4dc5c5b5ae55572d204c96cb0c7205c84e313089
mt_tarik <skorohodit12@gmail.com>
2023-06-22 12:02:42 +0300

    fix_bug

--- social-network-api/src/main/java/com/socialnetwork/api/configs/WebSocketConfiguration.java
+++ social-network-api/src/main/java/com/socialnetwork/api/configs/WebSocketConfiguration.java
@@ -12,12 +12,14 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
+
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+
         registry.addEndpoint("/websocket")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
