c7993844f89763500f1f0438f589480d2d0fb755
Anton <bahtin.antonik@gmail.com>
2023-07-04 22:58:11 +0300

    Add margin to container

--- social-network-ui/src/pages/Explore/Explore.jsx
+++ social-network-ui/src/pages/Explore/Explore.jsx
@@ -9,11 +9,12 @@ import TrendsSection from './TrendsSection';
 
 const Explore = () => {
   const matches = useMediaQuery((theme) => theme.breakpoints.up('md'));
+  const matchesSM = useMediaQuery((theme) => theme.breakpoints.down('sm'));
   const { user } = useSelector((state) => state.auth);
 
   return (
     <Grid container spacing={2}>
-      <Grid item xs={matches ? 7 : 12}>
+      <Grid item xs={matches ? 7 : 12} sx={{ mt: matchesSM ? '60px' : 0 }}>
         <Form action="/" method="post">
           <SearchInput />
         </Form>
