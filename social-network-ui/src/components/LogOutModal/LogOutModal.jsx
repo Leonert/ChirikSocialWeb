e709cc170ef17aeb0fc44c981b067288c24f918b
Anton <bahtin.antonik@gmail.com>
2023-07-04 22:50:55 +0300

    Fix logout modal icon

--- social-network-ui/src/components/LogOutModal/LogOutModal.jsx
+++ social-network-ui/src/components/LogOutModal/LogOutModal.jsx
@@ -1,12 +1,12 @@
 import { Button, Dialog } from '@material-ui/core';
-import { Box, Typography } from '@mui/material';
+import { Box, IconButton, Typography } from '@mui/material';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 import { handleLogOutModal } from '../../features/slices/authModalSlice';
 import { logoutUser } from '../../features/slices/authSlice';
-import { TweetIcon } from '../../icon';
+import { Chirick, TweetIcon } from '../../icon';
 import { useLogOutStyle } from './LogOutModalStyle';
 
 function LogOutModal() {
@@ -26,9 +26,23 @@ function LogOutModal() {
   return (
     <Dialog className={classes.content} open={openModal} onClose={onClose} aria-labelledby="form-dialog-title">
       <Box className={classes.pageItem}>
-        <div className={classes.logoIcon} style={{ display: 'flex', justifyContent: 'center' }}>
-          {TweetIcon}
-        </div>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center !important',
+            '& .MuiIconButton-root': {
+              maxWidth: '65px',
+              maxHeight: '65px',
+              '& svg': {
+                color: (theme) => theme.palette.primary.main,
+                height: '100%',
+                width: '100%',
+              },
+            },
+          }}
+        >
+          <IconButton>{Chirick}</IconButton>
+        </Box>
         <Typography className={classes.header} component="h2">
           Log out of Chirik?
         </Typography>
