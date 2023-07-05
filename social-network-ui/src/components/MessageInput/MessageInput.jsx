37b1d0e4354a1a192dad8f37db2ff63930ac5213
mt_tarik <skorohodit12@gmail.com>
2023-07-05 18:30:23 +0300

    fix

--- social-network-ui/src/components/MessageInput/MessageInput.jsx
+++ social-network-ui/src/components/MessageInput/MessageInput.jsx
@@ -1,23 +1,29 @@
-import TextField from "@material-ui/core/TextField/TextField";
-import {styled} from "@mui/material";
 
-export const MessageInput =styled(TextField)(({ theme }) => ({
 
+import { styled } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+
+export const MessageInput = styled(TextField)(({ theme }) => ({
     root: {
+        width: '100%',
         '& .MuiOutlinedInput-root': {
             marginTop: 2,
             borderRadius: 5,
-            padding: "10px 20px",
-            border: "1px solid rgb(37, 51, 65)",
-            width: 550,
+            padding: '10px 20px',
+            border: '1px solid rgb(37, 51, 65)',
             '&.Mui-focused': {
-                '& fieldset': { borderWidth: 1, borderColor: theme.palette.primary.dark },
+                '& fieldset': {
+                    borderWidth: 1,
+                    borderColor: theme.palette.primary.dark,
+                },
                 '& svg path': {
                     fill: theme.palette.primary.main,
                 },
             },
             '&:hover': {
-                '& fieldset': { borderColor: 'transparent' },
+                '& fieldset': {
+                    borderColor: 'transparent',
+                },
             },
             '& fieldset': {
                 borderColor: 'transparent',
@@ -25,11 +31,11 @@ export const MessageInput =styled(TextField)(({ theme }) => ({
             },
         },
         '& .MuiOutlinedInput-input': {
-            color: 'rgb(255,255,255)',
-
-            "&::placeholder": {
-                color: 'rgb(255,255,255)',
+            color: 'rgb(255, 255, 255)',
+            '&::placeholder': {
+                color: 'rgb(255, 255, 255)',
             },
         },
     },
 }));
+
