70d1e88e7d40f57133ca2d3385c85f289535ba64
Anton <bahtin.antonik@gmail.com>
2023-07-05 14:58:30 +0300

    Change endpoint

--- social-network-ui/src/util/constants.js
+++ social-network-ui/src/util/constants.js
@@ -1,5 +1,5 @@
 export const TOKEN = 'token';
-export const SOCKET_URL = 'https://chirik-fad33dd28d87.herokuapp.com/websocket';
+export const SOCKET_URL = 'https://chirikfp3-079978c808c1.herokuapp.com/websocket';
 export const MAX_BIO_LENGTH = 160;
 export const MAX_NAME_LENGTH = 50;
 export const MAX_LOCATION_LENGTH = 30;
